edbba5df79dd123fcdaa278f141c96fa9897c0cf
PuemMTH <puem.mth@gmail.com>
2024-02-25 11:07:57 +0700

    Refactor code for improved performance

---
+++ src/contents/header.js
@@ -0,0 +1,9 @@
+import filename from '../assets/CV.pdf';
+
+export const data = {
+    name:"Tanpat Eiam-arj",
+    title:"Software Developer | Full Stack Developer",
+    caption:"I am a software developer with a passion for creating and developing software. I am a quick learner and a team player. I am always looking for new opportunities to learn and grow.",
+    btnText:"View Resume",
+    link:filename,
+}
